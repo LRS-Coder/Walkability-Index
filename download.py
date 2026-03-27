c3d3625b3e80abd205a4ebdc5ee61c033739b24a
LRS-Coder <robert.larini@outlook.com>
2026-03-27 23:50:42 +0000

    added ability to see downloaded boundary

--- download.py
+++ download.py
@@ -9,7 +9,8 @@ folder = "data"
 # formats the location name for use a filename
 location_filename = location.replace(",", "").replace(" ", "_")
 
-# download buildings, amenities, and walking network
+# download boundary, buildings, amenities, and walking network
+boundary = ox.geocode_to_gdf(location)
 buildings = ox.features_from_place(location, tags = {"building": True})
 amenities = ox.features_from_place(location, tags = {"amenity": True})
 network = ox.graph.graph_from_place(location, network_type = "walk")
@@ -18,7 +19,8 @@ network = ox.graph.graph_from_place(location, network_type = "walk")
 # define target crs from network for buildings and amenities to match
 network_proj = ox.project_graph(network)
 target_crs = network_proj.graph['crs']
-# convert crs of buildings and amenities to match network
+# convert crs of boundary, buildings and amenities to match network
+boundary_proj = boundary.to_crs(target_crs)
 buildings_proj = buildings.to_crs(target_crs)
 amenities_proj = amenities.to_crs(target_crs)
 # save buildings
@@ -30,3 +32,7 @@ amenities_proj.to_file(amenities_file, driver = "GeoJSON")
 # save walking network
 network_file = os.path.join(folder, f"{location_filename}_network.graphml")
 ox.save_graphml(network_proj, network_file)
+
+# plot boundary on map
+m = boundary_proj.explore(name='boundary', color='black')
+m.save('boundary.html')
