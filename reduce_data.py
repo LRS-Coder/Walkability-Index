f7e8c7ae3dd9402381548c7fdae4f0f651717a52
LRS-Coder <robert.larini@outlook.com>
2026-04-05 17:47:36 +0100

    reduced data stored in geolytix files
    - kept id, branch/store name, longitude, and latitude for bank and supermarket data
    - only kept data for banks that are open
    - kept data representing the size of the supermarkets

---
+++ reduce_data.py
@@ -0,0 +1,15 @@
+# import modules
+import pandas as pd
+
+# remove unnecessary rows and columns from csvs and save
+# desired columns are id, branch_name, long_wgs84, lat_wgs84 for open banks
+(
+    pd.read_csv("data/geolytix_uk_open_bank_branches.csv")
+    .loc[lambda df: df["status"] == "Open", ["id","branch_name","long_wgs84","lat_wgs84"]]
+    .to_csv("data/geolytix_uk_open_bank_branches.csv", index = False)
+)
+# desired columns are id, store_name, size_band long_wgs, lat_wgs for all supermarkets
+(
+    pd.read_csv("data/geolytix_retailpoints_v40_202601.csv")[["id","store_name","size_band","long_wgs","lat_wgs"]]
+    .to_csv("data/geolytix_retailpoints_v40_202601.csv", index = False)
+)
