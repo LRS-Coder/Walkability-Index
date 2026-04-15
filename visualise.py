06e2ee49ce2663253b9041047e71caca839899f6
LRS-Coder <robert.larini@outlook.com>
2026-04-15 22:57:45 +0100

    updated visulaise.py to show building scores
    - buildings are now colour mapped based on their walkability score (Overall Score)
    - building tooltips provide information about their score in each category
    - which set of scores to use will update the folium map
    - changed which base map the map will begin in (From Esri WorldImagery to CartoDB positron)

--- visualise.py
+++ visualise.py
@@ -6,6 +6,7 @@ import tempfile
 import webbrowser
 import pandas as pd
 import geopandas as gpd
+import branca.colormap as cm
 
 # define function for adding amenity markers to a folium map as groups
 def add_amenity_markers(row, amenity_groups, icons_dictionary):
@@ -33,14 +34,66 @@ def add_amenity_markers(row, amenity_groups, icons_dictionary):
     color, icon = icons_dictionary[row.group]
     folium.Marker(location=coords, popup=folium.Popup(popup_text,max_width=250), icon=folium.Icon(color=color, icon=icon, prefix='fa')).add_to(amenity_groups[row['group']])
 
+# define a function for creating the style function for add_walkability_buildings
+def make_style_function(colormap, walkability):
+
+    # utilise an inner function to remember colormap and walkability
+    def style_function(feature):
+
+        # extract walkability index
+        val = feature['properties'].get(walkability)
+
+        # convert value into a colour and return it with the other styling instructions
+        return {
+            'fillColor': colormap(val),
+            'color': 'black',
+            'fillOpacity': 1,
+            'weight': 1,
+        }
+
+    # return the inner function
+    return style_function
+
+# define function for adding buildings to folium map with scoring for a given distance, and colouring buildings based on overall score
+def add_walkability_buildings(buildings, scores, t=15):
+    # define the name of the walkability index based on selected walking time
+    walkability = f'{t} Overall'
+
+    # set the fields and aliases to be added to the building tool tip in the map
+    hover_fields = [f'{t} {s}' for s in scores]
+    hover_aliases = [f'{s} ({t} min)' for s in scores]
+
+    # define the tooltip using the fields and aliases previously defined
+    tooltip = folium.GeoJsonTooltip(fields = hover_fields, aliases = hover_aliases, sticky = True)
+
+    # define the colourmap to match the expected scale of the walkability score and set caption
+    colormap = cm.linear.viridis.scale(0,100)
+    colormap.caption = f'{t}-minute Walkability Score'
+
+    # plot the buildings with their associated data onto the folium map
+    folium.GeoJson(
+        buildings,
+        name = f'Buildings with {t}-minute Walkability Score',
+        style_function = make_style_function(colormap, walkability),
+        tooltip = tooltip
+    ).add_to(m)
+
+    # add the colourmap to the folium map
+    colormap.add_to(m)
+
+    return m
+
 # define subfolder
-subfolder = 'data/Belfast, Northern Ireland/'
+subfolder = 'data/Coleraine, Northern Ireland/'
 
 # read data files
 amenities = gpd.read_file(os.path.join(subfolder,'amenities.geojson'))
-buildings = gpd.read_file(os.path.join(subfolder,'buildings.geojson'))
+buildings = gpd.read_file(os.path.join(subfolder,'buildings_scored.geojson'))
 network = ox.load_graphml(os.path.join(subfolder,'network.graphml'))
 nodes, edges = ox.graph_to_gdfs(network)
+
+# define selection of walkability time (can be 15, 30, or 60)
+selection = 15
 
 # define amenity groups
 amenity_groups = {
@@ -103,10 +156,11 @@ minx, miny, maxx, maxy = buildings_to_plot.total_bounds
 m = folium.Map(location=[(miny + maxy)/2,(minx + maxx)/2], tiles=None, control_scale=True)
 m.fit_bounds([[miny,minx],[maxy,maxx]])
 
+# define a list of the names of the scoring indexes to use in building tooltips
+scores = ['Overall'] + list(amenity_groups.keys())
+
 # plot buildings onto a folium map
-bg = folium.FeatureGroup(name='Buildings', show=True)
-folium.GeoJson(buildings_to_plot[['id','geometry']],style_function=lambda feature: {"color": "darkred"}).add_to(bg)
-bg.add_to(m)
+add_walkability_buildings(buildings_to_plot, scores, 15)
 
 # plot walking network onto a folium map
 wng = folium.FeatureGroup(name='Walking Network', show=True)
@@ -114,18 +168,18 @@ folium.GeoJson(edges_to_plot[['osmid','geometry']],style_function=lambda feature
 wng.add_to(m)
 
 # assign markers as groups to be plotted instead of plotting directly
-amenity_groups = {}
+amenity_groupings = {}
 for group in amenities_to_plot['group'].unique():
     ag = folium.FeatureGroup(name=f'Amenities: {group}', show=False)
     ag.add_to(m)
-    amenity_groups[group] = ag
+    amenity_groupings[group] = ag
 
 # plot amenities onto a folium map as groups
-amenities_to_plot.apply(add_amenity_markers, axis=1, amenity_groups=amenity_groups, icons_dictionary=icons_dictionary)
+amenities_to_plot.apply(add_amenity_markers, axis=1, amenity_groups=amenity_groupings, icons_dictionary=icons_dictionary)
 
 # add base maps to folium map
-folium.TileLayer(tiles='CartoDB positron', name='Light Map (CartoDB').add_to(m)
 folium.TileLayer(tiles='Esri WorldImagery', name='Satellite (Esri)').add_to(m)
+folium.TileLayer(tiles='CartoDB positron', name='Light Map (CartoDB)').add_to(m)
 folium.LayerControl().add_to(m)
 
 # create a temporary file to for folium image
