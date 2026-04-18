a449e39d4ce42540c0af7782bdd23b4a63455cf7
LRS-Coder <robert.larini@outlook.com>
2026-04-18 13:40:59 +0100

    utilise config import
    - import amenity_groups from config instead of explicitly stating

--- visualise.py
+++ visualise.py
@@ -7,6 +7,7 @@ import webbrowser
 import pandas as pd
 import geopandas as gpd
 import branca.colormap as cm
+from config import amenity_groups
 
 # define function for adding amenity markers to a folium map as groups
 def add_amenity_markers(row, amenity_groups, icons_dictionary):
@@ -94,27 +95,6 @@ nodes, edges = ox.graph_to_gdfs(network)
 
 # define selection of walkability time (can be 15, 30, or 60)
 selection = 15
-
-# define amenity groups
-amenity_groups = {
-    'Education': ['school','outdoor_education_centre','college','university','kindergarten'],
-    'Food & Drink': ['fast_food','restaurant','cafe','pub','bar','ice_cream','food_court','vending_machine','drinking_water',
-                     'water_point'],
-    'Groceries': ['small_supermarket','medium_supermarket','large_supermarket','hypermarket','marketplace'],
-    'Postal': ['post_office','post_box'],
-    'Banking': ['atm','payment_terminal','bank','moneylender','money_lender','bureau_de_change'],
-    'Religion': ['place_of_worship','monastery'],
-    'Entertainment': ['community_centre','casino','concert_hall','cinema','theatre','library','tanning_salon',
-                      'adult_gaming_centre','trampoline_park','bowling_alley','miniature_golf','sports_centre',
-                      'fitness_centre','marina','indoor_play','events_venue','arts_centre','music_venue',
-                      'studio','nightclub','dance','social_centre','conference_centre','exhibition_centre','golf_course',
-                      'fitness_station','bird_hide','swimming_pool','stadium','gambling','sauna','amusement_arcade',
-                      'music_school','escape_game','fishing','hackerspace','hookah_lounge'],
-    'Healthcare': ['dentist','pharmacy','doctors','clinic','hospital'],
-    'Public Services': ['fire_station','police','townhall','courthouse','ranger_station'],
-    'Public Transport': ['taxi','bus_stop','bus_station','train_station','train_and_bus_station','ferry_terminal'],
-    'Dedicated Greenspaces': ['pitch','park','playground','garden','track','firepit','grave_yard','nature_reserve','dog_park']
-}
 
 # assign group to amenities
 amenity_to_group = {amenity:group
